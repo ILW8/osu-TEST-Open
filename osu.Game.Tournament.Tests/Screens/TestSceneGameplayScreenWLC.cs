59557162a4d026c14bf70dda53b88b5ec42b0067
Dao Heng Liu <daohengliu@gmail.com>
2023-11-17 18:09:20 -0600

    use randomized userids for pfp test scenes

--- osu.Game.Tournament.Tests/Screens/TestSceneGameplayScreenWLC.cs
+++ osu.Game.Tournament.Tests/Screens/TestSceneGameplayScreenWLC.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
@@ -29,15 +30,28 @@ namespace osu.Game.Tournament.Tests.Screens
         {
             AddStep("update users", () =>
             {
-                var newPlayers = new BindableList<TournamentUser>();
-                Ladder.CurrentMatch.Value?.Team1.Value?.Players.BindTo(newPlayers);
-                newPlayers.Clear();
-                newPlayers.AddRange(new List<TournamentUser>
+                var knownGoodUsersList = new List<TournamentUser>
                 {
-                    new TournamentUser { OnlineID = 14167692 },
-                    new TournamentUser { OnlineID = 5182050 },
-                    new TournamentUser { OnlineID = 9501251 },
-                });
+                    new TournamentUser { OnlineID = 6447454 },
+                    new TournamentUser { OnlineID = 6600930 },
+                    new TournamentUser { OnlineID = 7562902 },
+                    new TournamentUser { OnlineID = 9269034 },
+                    new TournamentUser { OnlineID = 11367222 },
+                    new TournamentUser { OnlineID = 12779141 },
+                    new TournamentUser { OnlineID = 10549880 },
+                    new TournamentUser { OnlineID = 13211727 },
+                    new TournamentUser { OnlineID = 10323184 },
+                };
+                var newPlayers1 = new BindableList<TournamentUser>();
+                var newPlayers2 = new BindableList<TournamentUser>();
+                Ladder.CurrentMatch.Value?.Team1.Value?.Players.BindTo(newPlayers1);
+                Ladder.CurrentMatch.Value?.Team2.Value?.Players.BindTo(newPlayers2);
+                newPlayers1.Clear();
+                newPlayers2.Clear();
+
+                Random rnd = new Random();
+                newPlayers1.AddRange(knownGoodUsersList.OrderBy(x => rnd.Next()).Take(3));
+                newPlayers2.AddRange(knownGoodUsersList.OrderBy(x => rnd.Next()).Take(3));
             });
             createScreen();
             toggleWarmup();
