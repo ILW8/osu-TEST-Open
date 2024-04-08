409157f7db9251e3c5e96b598e23f3eed136d5af
Dao Heng Liu <daohengliu@gmail.com>
2024-04-08 08:48:01 +0100

    keep spectator state when exiting results screen manually

--- osu.Game/Screens/OnlinePlay/Multiplayer/Spectate/MultiSpectatorScreen.cs
+++ osu.Game/Screens/OnlinePlay/Multiplayer/Spectate/MultiSpectatorScreen.cs
@@ -360,7 +360,7 @@ namespace osu.Game.Screens.OnlinePlay.Multiplayer.Spectate
 
             // // On a manual exit, set the player back to idle unless gameplay has finished.
             // // Of note, this doesn't cover exiting using alt-f4 or menu home option.
-            if (multiplayerClient.Room.State == MultiplayerRoomState.Open)
+            if (multiplayerClient.Room.State is MultiplayerRoomState.Open or MultiplayerRoomState.Results)
                 return base.OnBackButton();
 
             multiplayerClient.ManualExitRequested = true;
