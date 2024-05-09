bc1f4fd57aa5bdaed127a5cd0e68f4544beeccaa
ILW8 <daohengliu@gmail.com>
2024-05-09 02:10:23 +0100

    add check to ensure at least one player is ready for mp start

--- osu.Game/Online/Chat/StandAloneChatDisplay.cs
+++ osu.Game/Online/Chat/StandAloneChatDisplay.cs
@@ -499,6 +499,14 @@ namespace osu.Game.Online.Chat
                                 break;
                             }
 
+                            // no one is ready, server won't allow starting the map
+                            if (Client.Room?.Users.All(u => u.State != MultiplayerUserState.Ready) ?? false)
+                            {
+                                Logger.Log(@"Tried to start match when no player is ready. Cancelling!", LoggingTarget.Runtime, LogLevel.Important);
+                                botMessageQueue.Enqueue(new Tuple<string, Channel>(@"No player ready, cannot start match.", Channel.Value));
+                                break;
+                            }
+
                             Client.StartMatch().FireAndForget();
                             break;
                     }
