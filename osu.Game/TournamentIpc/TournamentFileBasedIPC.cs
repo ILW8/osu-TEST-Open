a0c9b42a5670cdeb26868d8aaf7ae6073a4c7110
Dao Heng Liu <daohengliu@gmail.com>
2024-07-06 05:26:56 +0100

    write chat contents to ipc-chat.txt

--- osu.Game/TournamentIpc/TournamentFileBasedIPC.cs
+++ osu.Game/TournamentIpc/TournamentFileBasedIPC.cs
@@ -2,6 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
+using System.IO;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Logging;
@@ -11,8 +12,18 @@ using osu.Game.Online.Chat;
 
 namespace osu.Game.TournamentIpc
 {
+    // am I being paranoid with the locks? Not familiar with threading model in C#
     public partial class TournamentFileBasedIPC : Component
     {
+        private const string file_ipc_filename = @"ipc.txt";
+        private const string file_ipc_state_filename = @"ipc-state.txt";
+        private const string file_ipc_scores_filename = @"ipc-scores.txt";
+        private const string file_ipc_chat_filename = @"ipc-chat.txt";
+
+        private readonly object mtx = new object();
+        private bool dirty = false;
+
+        private Storage tournamentStorage = null!;
         private ScheduledDelegate? scheduled;
 
         private readonly List<Message> chatMessages = new List<Message>();
@@ -20,22 +31,46 @@ namespace osu.Game.TournamentIpc
         [BackgroundDependencyLoader]
         private void load(Storage storage)
         {
-            var tournamentStorage = storage.GetStorageForDirectory(@"tournaments");
+            tournamentStorage = storage.GetStorageForDirectory(@"tournaments");
+
             scheduled?.Cancel();
             scheduled = Scheduler.AddDelayed(delegate
             {
-                Logger.Log(@$"chat has {chatMessages.Count} messages", LoggingTarget.Runtime, LogLevel.Debug);
-            }, 250, true);
+                lock (mtx)
+                {
+                    Logger.Log(@$"chat has {chatMessages.Count} messages", LoggingTarget.Runtime, LogLevel.Debug);
+                    if (!dirty)
+                        return;
+
+                    using (var chatIpcStream = tournamentStorage.CreateFileSafely(file_ipc_chat_filename))
+                    {
+                        var sw = new StreamWriter(chatIpcStream);
+
+                        foreach (var message in chatMessages)
+                        {
+                            sw.Write($"{message.Timestamp.ToUnixTimeMilliseconds()},{message.Sender.Username},{message.Sender.Id},{message.Content}\n");
+                        }
+
+                        sw.Close();
+                    }
+
+                    dirty = false;
+                }
+            }, 500, true);
         }
 
         public void AddChatMessage(Message message)
         {
             chatMessages.Add(message);
+            lock (mtx)
+                dirty = true;
         }
 
         public void ClearChatMessages()
         {
             chatMessages.Clear();
+            lock (mtx)
+                dirty = true;
         }
     }
 }
