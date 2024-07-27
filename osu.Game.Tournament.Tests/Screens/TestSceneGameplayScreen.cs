e542a1a5d4c18219018266c93c824af089f9fd1a
Dao Heng Liu <daohengliu@gmail.com>
2024-07-27 03:02:31 +0100

    add cumulative score delta display

--- osu.Game.Tournament.Tests/Screens/TestSceneGameplayScreen.cs
+++ osu.Game.Tournament.Tests/Screens/TestSceneGameplayScreen.cs
@@ -1,11 +1,13 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Testing;
+using osu.Framework.Utils;
 using osu.Game.Tournament.Components;
 using osu.Game.Tournament.IPC;
 using osu.Game.Tournament.Screens.Gameplay;
@@ -69,6 +71,31 @@ namespace osu.Game.Tournament.Tests.Screens
                 LazerIPCInfo.Score2.Value = 63_727;
             });
             AddStep("set state: ranking", () => LazerIPCInfo.State.Value = TourneyState.Ranking);
+        }
+
+        [Test]
+        public void TestScoreCumulativeDelta()
+        {
+            AddStep("enable cumulative score", () => Ladder.CumulativeScore.Value = true);
+
+            createScreen();
+            toggleWarmup();
+
+            for (int i = 0; i < 7; i++)
+            {
+                AddStep($"add map {i + 1} results", () =>
+                {
+                    Ladder.CurrentMatch.Value!.Team1Score.Value += RNG.Next(1_000_000);
+                    Ladder.CurrentMatch.Value!.Team2Score.Value += RNG.Next(1_000_000);
+                });
+
+                AddUntilStep("wait for score delta to settle", () =>
+                {
+                    var scoreDeltaDrawable = this.ChildrenOfType<MatchHeader.MatchCumulativeScoreDiffCounter>().First();
+                    return Math.Abs(scoreDeltaDrawable.DisplayedCount - scoreDeltaDrawable.Current.Value) < 0.0001f;
+                });
+                AddRepeatStep("wait a bit more", () => { }, 8);
+            }
         }
 
         [Test]
--- osu.Game.Tournament/Screens/Gameplay/Components/MatchHeader.cs
+++ osu.Game.Tournament/Screens/Gameplay/Components/MatchHeader.cs
@@ -1,9 +1,15 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using osu.Framework.Allocation;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Sprites;
+using osu.Game.Graphics.UserInterface;
 using osu.Game.Tournament.Components;
 using osu.Game.Tournament.Models;
 using osuTK;
@@ -12,9 +18,26 @@ namespace osu.Game.Tournament.Screens.Gameplay.Components
 {
     public partial class MatchHeader : Container
     {
+        private const int cumulative_delta_height = 24;
+
+        public partial class MatchCumulativeScoreDiffCounter : CommaSeparatedScoreCounter
+        {
+            protected override OsuSpriteText CreateSpriteText() => base.CreateSpriteText().With(s =>
+            {
+                s.Spacing = new Vector2(-2);
+                s.Font = OsuFont.Torus.With(weight: FontWeight.Regular, size: cumulative_delta_height, fixedWidth: true);
+            });
+        }
+
         private TeamScoreDisplay teamDisplay1 = null!;
         private TeamScoreDisplay teamDisplay2 = null!;
         private DrawableTournamentHeaderLogo logo = null!;
+        private MatchCumulativeScoreDiffCounter cumulativeScoreDiffCounter = null!;
+        private readonly Bindable<TournamentMatch?> currentMatch = new Bindable<TournamentMatch?>();
+        private readonly Bindable<long?> team1Score = new Bindable<long?>();
+        private readonly Bindable<long?> team2Score = new Bindable<long?>();
+        private SpriteIcon leftWinningTriangle = null!;
+        private SpriteIcon rightWinningTriangle = null!;
 
         private bool showScores = true;
 
@@ -51,8 +74,14 @@ namespace osu.Game.Tournament.Screens.Gameplay.Components
         }
 
         [BackgroundDependencyLoader]
-        private void load()
+        private void load(LadderInfo ladder)
         {
+            currentMatch.BindTo(ladder.CurrentMatch);
+            currentMatch.BindValueChanged(matchChanged, true);
+
+            team1Score.BindValueChanged(_ => updateScoreDelta());
+            team2Score.BindValueChanged(_ => updateScoreDelta());
+
             RelativeSizeAxes = Axes.X;
             Height = 95;
             Children = new Drawable[]
@@ -82,6 +111,49 @@ namespace osu.Game.Tournament.Screens.Gameplay.Components
                             Origin = Anchor.TopCentre,
                             Scale = new Vector2(0.4f)
                         },
+                        new FillFlowContainer
+                        {
+                            Direction = FillDirection.Horizontal,
+                            Anchor = Anchor.TopCentre,
+                            Origin = Anchor.TopCentre,
+                            Spacing = new Vector2(16),
+                            Children = new Drawable[]
+                            {
+                                leftWinningTriangle = new SpriteIcon
+                                {
+                                    Icon = FontAwesome.Solid.ChevronLeft,
+                                    Width = 12,
+                                    Height = 12,
+                                    Margin = new MarginPadding
+                                    {
+                                        Vertical = 7
+                                    },
+                                    Alpha = 0,
+                                    Anchor = Anchor.TopCentre,
+                                    Origin = Anchor.TopCentre,
+                                    AlwaysPresent = true
+                                },
+                                cumulativeScoreDiffCounter = new MatchCumulativeScoreDiffCounter
+                                {
+                                    Anchor = Anchor.TopCentre,
+                                    Origin = Anchor.TopCentre,
+                                },
+                                rightWinningTriangle = new SpriteIcon
+                                {
+                                    Icon = FontAwesome.Solid.ChevronRight,
+                                    Width = 12,
+                                    Height = 12,
+                                    Margin = new MarginPadding
+                                    {
+                                        Vertical = 7
+                                    },
+                                    Alpha = 0,
+                                    Anchor = Anchor.TopCentre,
+                                    Origin = Anchor.TopCentre,
+                                    AlwaysPresent = true
+                                },
+                            }
+                        }
                     }
                 },
                 teamDisplay1 = new TeamScoreDisplay(TeamColour.Red)
@@ -97,6 +169,34 @@ namespace osu.Game.Tournament.Screens.Gameplay.Components
             };
         }
 
+        private void updateScoreDelta()
+        {
+            long scoreDelta = team1Score.Value - team2Score.Value ?? 0;
+
+            cumulativeScoreDiffCounter.Current.Value = Math.Abs(scoreDelta);
+
+            leftWinningTriangle.FadeTo(scoreDelta > 0 ? 1 : 0, 200);
+            rightWinningTriangle.FadeTo(scoreDelta < 0 ? 1 : 0, 200);
+        }
+
+        private void matchChanged(ValueChangedEvent<TournamentMatch?> match)
+        {
+            team1Score.UnbindBindings();
+            team2Score.UnbindBindings();
+
+            Scheduler.AddOnce(updateMatch);
+        }
+
+        private void updateMatch()
+        {
+            var match = currentMatch.Value;
+
+            if (match == null) return;
+
+            team1Score.BindTo(match.Team1Score);
+            team2Score.BindTo(match.Team2Score);
+        }
+
         protected override void LoadComplete()
         {
             base.LoadComplete();
@@ -107,6 +207,7 @@ namespace osu.Game.Tournament.Screens.Gameplay.Components
         {
             teamDisplay1.ShowScore = showScores;
             teamDisplay2.ShowScore = showScores;
+            cumulativeScoreDiffCounter.FadeTo(showScores ? 1 : 0, 200);
 
             logo.Alpha = showLogo ? 1 : 0;
         }
